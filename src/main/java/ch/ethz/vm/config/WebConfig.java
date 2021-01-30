bd1fa1a0822b83da586a73ca8f75dcfb460065a6
Chaoran Chen <mail@chaoran-chen.de>
2021-01-30 23:39:24 +0100

    Allow CORS requests permanently
    The server provide read-only public data, there is no reason why CORS requests should not be permitted.

--- src/main/java/ch/ethz/vm/config/WebConfig.java
+++ src/main/java/ch/ethz/vm/config/WebConfig.java
@@ -1,7 +1,5 @@
 package ch.ethz.vm.config;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,14 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
-    Logger logger = LoggerFactory.getLogger(WebConfig.class);
-
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        if ("true".equals(System.getenv("VM_DEVELOPMENT"))) {
-            logger.info("Server is running in development mode. CORS requests are enabled.");
-            registry.addMapping("/**");
-        }
+        registry.addMapping("/**");
     }
 }
