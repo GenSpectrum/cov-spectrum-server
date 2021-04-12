f46622d5fc55c727681840b3f14d85fabfb33bba
Chaoran Chen <mail@chaoran-chen.de>
2021-04-12 12:52:44 +0200

    Pre-compute frequently used values for the sample endpoint

---
+++ src/main/java/ch/ethz/covspectrum/entity/core/SampleSelectionCacheKey.java
@@ -0,0 +1,148 @@
+package ch.ethz.covspectrum.entity.core;
+
+import org.javatuples.Pair;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+public class SampleSelectionCacheKey {
+
+    private final String fields;
+
+    private final boolean privateVersion;
+
+    private final String region;
+
+    private final String country;
+
+    private final String mutations;
+
+    private final float matchPercentage;
+
+    private final String dataType;
+
+    private final LocalDate dateFrom;
+
+    private final LocalDate dateTo;
+
+
+    public SampleSelectionCacheKey(
+            String fields,
+            boolean privateVersion,
+            String region,
+            String country,
+            String mutations,
+            float matchPercentage,
+            String dataType,
+            LocalDate dateFrom,
+            LocalDate dateTo
+    ) {
+        this.fields = fields;
+        this.privateVersion = privateVersion;
+        this.region = region;
+        this.country = country;
+        this.mutations = mutations;
+        this.matchPercentage = matchPercentage;
+        this.dataType = dataType;
+        this.dateFrom = dateFrom;
+        this.dateTo = dateTo;
+    }
+
+
+    public String getFields() {
+        return fields;
+    }
+
+
+    public boolean isPrivateVersion() {
+        return privateVersion;
+    }
+
+
+    public String getRegion() {
+        return region;
+    }
+
+
+    public String getCountry() {
+        return country;
+    }
+
+
+    public String getMutations() {
+        return mutations;
+    }
+
+
+    public float getMatchPercentage() {
+        return matchPercentage;
+    }
+
+
+    public String getDataType() {
+        return dataType;
+    }
+
+
+    public LocalDate getDateFrom() {
+        return dateFrom;
+    }
+
+
+    public LocalDate getDateTo() {
+        return dateTo;
+    }
+
+
+    public static SampleSelectionCacheKey fromSampleSelection(SampleSelection selection, String fields) {
+        String mutationsString = "";
+        if (selection.getVariant() != null) {
+            mutationsString = selection.getVariant().getMutations().stream()
+                    .map(AAMutation::decode)
+                    .map(AAMutationDecoded::toString)
+                    .sorted()
+                    .collect(Collectors.joining(","));
+        }
+        String dataType = "";
+        if (selection.getDataType() != null) {
+            dataType = selection.getDataType().toString();
+        }
+        return new SampleSelectionCacheKey(
+                fields,
+                selection.isUsePrivate(),
+                Objects.requireNonNullElse(selection.getRegion(), ""),
+                Objects.requireNonNullElse(selection.getCountry(), ""),
+                mutationsString,
+                selection.getMatchPercentage(),
+                dataType,
+                Objects.requireNonNullElse(selection.getDateFrom(), LocalDate.of(1990, 1, 1)),
+                Objects.requireNonNullElse(selection.getDateFrom(), LocalDate.of(1990, 1, 1))
+        );
+    }
+
+
+    /**
+     *
+     * @return (sample selection, fields)
+     */
+    public Pair<SampleSelection, String> toSampleSelection() {
+        SampleSelection selection = new SampleSelection()
+                .setUsePrivate(privateVersion)
+                .setRegion(!region.equals("") ? region : null)
+                .setCountry(!country.equals("") ? region : null)
+                .setMatchPercentage(matchPercentage)
+                .setDateFrom(!dateFrom.equals(LocalDate.of(1990, 1, 1)) ? dateFrom : null)
+                .setDateTo(!dateTo.equals(LocalDate.of(1990, 1, 1)) ? dateTo : null);
+        if (!mutations.equals("")) {
+            selection.setVariant(new Variant(Arrays.stream(mutations.split(","))
+                    .map(AAMutation::new)
+                    .collect(Collectors.toSet())));
+        }
+        if (!dataType.equals("")) {
+            selection.setDataType(DataType.valueOf(dataType));
+        }
+        return new Pair<>(selection, fields);
+    }
+}
