f99954f736b1eb804db96d7d590726538a9bee12
Chaoran Chen <mail@chaoran-chen.de>
2021-10-12 11:24:24 +0200

    Allow CORS

---
+++ src/main/kotlin/ch/ethz/covspectrum/config/WebConfig.kt
@@ -0,0 +1,12 @@
+package ch.ethz.covspectrum.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+    }
+}
