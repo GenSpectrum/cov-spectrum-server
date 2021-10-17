3210ada5d9239f50e0f4843ca9f1d5358a6effde
Chaoran Chen <mail@chaoran-chen.de>
2021-10-17 18:43:05 +0200

    Use the ISO date format as default

--- src/main/kotlin/ch/ethz/covspectrum/config/WebConfig.kt
+++ src/main/kotlin/ch/ethz/covspectrum/config/WebConfig.kt
@@ -1,12 +1,21 @@
 package ch.ethz.covspectrum.config
 
 import org.springframework.context.annotation.Configuration
+import org.springframework.format.FormatterRegistry
+import org.springframework.format.datetime.standard.DateTimeFormatterRegistrar
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
 
 @Configuration
 class WebConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
+    }
+
+    override fun addFormatters(registry: FormatterRegistry) {
+        val registrar = DateTimeFormatterRegistrar()
+        registrar.setUseIsoFormat(true)
+        registrar.registerFormatters(registry)
     }
 }
