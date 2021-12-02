6376c29bafc2f79d66fcf033f31279dc579d5ea9
Chaoran Chen <mail@chaoran-chen.de>
2021-12-02 13:33:21 +0100

    Fix /my-country endpoint if the server is behind Cloudflare

--- src/main/kotlin/ch/ethz/covspectrum/controller/IpLocationController.kt
+++ src/main/kotlin/ch/ethz/covspectrum/controller/IpLocationController.kt
@@ -1,6 +1,7 @@
 package ch.ethz.covspectrum.controller
 
 import ch.ethz.covspectrum.entity.res.IpCountryResponse
+import ch.ethz.covspectrum.service.DatabaseService
 import com.maxmind.geoip2.DatabaseReader
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.RequestMapping
@@ -12,16 +13,25 @@ import javax.servlet.http.HttpServletRequest
 
 @RestController
 @RequestMapping("/internal")
-class IpLocationController {
+class IpLocationController(
+    private val databaseService: DatabaseService
+) {
 
     private val geoLite2DBFilePath: String? = System.getenv("COV_SPECTRUM_GEO_LITE2_DB_PATH");
     private val ipDatabaseReader: DatabaseReader? = if (geoLite2DBFilePath == null) null else
         DatabaseReader.Builder(File(geoLite2DBFilePath)).build()
+    private val isoToCountryMap = fetchIsoToCountryMapping()
 
     @RequestMapping(value = ["/my-country"], produces = [MediaType.APPLICATION_JSON_VALUE])
     fun getMyCountry(request: HttpServletRequest): IpCountryResponse {
         if (ipDatabaseReader == null) {
             return IpCountryResponse(null, null)
+        }
+        // If the server is running behind Cloudflare, the country of the user is in the "CF-IPCountry" header field.
+        // https://support.cloudflare.com/hc/en-us/articles/200168236-Configuring-Cloudflare-IP-Geolocation
+        val cfCountry = request.getHeader("CF-IPCountry")
+        if (cfCountry != null) {
+            return isoToCountryMap[cfCountry] ?: IpCountryResponse(null, null)
         }
         // If the server is running behind a reverse proxy, it is required that the reverse proxy sets the
         // X-Real-IP header to the IP address of the visitor.
@@ -33,6 +43,34 @@ class IpLocationController {
         } catch (e: Exception) {
             return IpCountryResponse(null, null)
         }
+    }
+
+    private fun fetchIsoToCountryMapping(): Map<String, IpCountryResponse> {
+        val mapping = mutableMapOf<String, IpCountryResponse>();
+        val sql = """
+            select distinct
+              iso_alpha_2,
+              cov_spectrum_country,
+              cov_spectrum_region
+            from spectrum_country_mapping
+            where cov_spectrum_country is not null and iso_alpha_2 is not null;
+        """.trimIndent()
+        databaseService.getConnection().use { conn ->
+            conn.createStatement().use { statement ->
+                statement.executeQuery(sql).use { rs ->
+                    while (rs.next()) {
+                        mapping.put(
+                            rs.getString("iso_alpha_2"),
+                            IpCountryResponse(
+                                rs.getString("cov_spectrum_region"),
+                                rs.getString("cov_spectrum_country")
+                            )
+                        )
+                    }
+                }
+            }
+        }
+        return mapping
     }
 
 }
