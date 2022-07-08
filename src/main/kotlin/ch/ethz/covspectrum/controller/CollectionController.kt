a08c85b4fa46bc6d4ab2fcc78f87f876777718e5
Chaoran Chen <mail@chaoran-chen.de>
2022-07-08 15:42:17 +0200

    Change path of collection endpoints

--- src/main/kotlin/ch/ethz/covspectrum/controller/CollectionController.kt
+++ src/main/kotlin/ch/ethz/covspectrum/controller/CollectionController.kt
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/collection")
+@RequestMapping("/resource/collection")
 class CollectionController(
     private val databaseService: DatabaseService
 ) {
