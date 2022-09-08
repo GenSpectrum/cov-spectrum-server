6c6fd9de5b41f64d655815b51b70e9069d02537b
Chaoran Chen <mail@chaoran-chen.de>
2022-09-08 22:06:52 +0200

    Update JVM, Gradle and Kotlin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,13 +4,13 @@ plugins {
     id("org.springframework.boot") version "2.5.5"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     id("org.ec4j.editorconfig") version "0.0.3"
-    kotlin("jvm") version "1.5.31"
-    kotlin("plugin.spring") version "1.5.31"
+    kotlin("jvm") version "1.7.10"
+    kotlin("plugin.spring") version "1.7.10"
 }
 
 group = "ch.ethz"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_15
+java.sourceCompatibility = JavaVersion.VERSION_17
 
 repositories {
     mavenCentral()
@@ -36,7 +36,7 @@ dependencies {
 tasks.withType<KotlinCompile> {
     kotlinOptions {
         freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "15"
+        jvmTarget = "17"
     }
 }
 
