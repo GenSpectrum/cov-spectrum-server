79787b24607eae0d0922194889a8d84be5f3a3cb
Chaoran Chen <mail@chaoran-chen.de>
2023-06-08 15:40:46 +0200

    feat: add table for wastewater VILOCA results
    Related to https://github.com/GenSpectrum/cov-spectrum-website/issues/813

---
+++ src/main/resources/db/migration/V8__add-wastewater-viloca.sql
@@ -0,0 +1,5 @@
+-- See https://github.com/GenSpectrum/cov-spectrum-website/issues/813
+-- and https://github.com/LaraFuhrmann/SARS-CoV-2-wastewater-sample-processing-VILOCA
+create table wastewater_viloca (
+  plot_svg text not null
+);
